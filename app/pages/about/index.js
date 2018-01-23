6b99daa5e615344d3b83016d3738aba47e29cf72
xiewang <1010658096@qq.com>
2018-01-23 22:44:37 +0800

    set version

--- app/pages/about/index.js
+++ app/pages/about/index.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import {
     View,
     Text,
@@ -13,10 +13,12 @@ import {
 } from 'react-native';
 import styles from './style';
 import Toolbar from '../../components/toolbar';
-import {Token,request,toast } from '../../utils/common';
+import {Token, request, toast} from '../../utils/common';
 import deprecatedComponents from 'react-native-deprecated-custom-components';
 import Icon from '../../../node_modules/react-native-vector-icons/FontAwesome';
 import H5Page from '../h5';
+
+const pkg = require('../../../package.json');
 
 const Navigator = deprecatedComponents.Navigator;
 const logo = require('../../assets/logo/logo.png');
@@ -27,13 +29,13 @@ class About extends React.Component {
         super(props);
         this._gotoH5Page = this._gotoH5Page.bind(this);
         this.state = {
-            version: '1.1.0'
+            version: pkg.version ? pkg.version : '1.1.0'
         };
     }
 
-    _gotoH5Page(title, uri){
-        const { navigator } = this.props;
-        if(navigator) {
+    _gotoH5Page(title, uri) {
+        const {navigator} = this.props;
+        if (navigator) {
             navigator.push({
                 name: 'H5Page',
                 component: H5Page,
@@ -43,52 +45,52 @@ class About extends React.Component {
         }
     }
 
-
-
     render() {
         return (
-            <TouchableWithoutFeedback style={{flex:1}} >
+            <TouchableWithoutFeedback style={{flex: 1}}>
 
                 <View style={[styles.container, Platform.OS === 'android' ? null : {marginTop: 21}]}>
                     <Toolbar
                         title="关于剁手记"
                         navigator={this.props.navigator}
                         hideDrop={true}
-                        />
+                    />
                     <View style={styles.content}>
                         <View>
                             <Image style={styles.logo} source={logo}></Image>
                         </View>
                         <View style={styles.version}>
-                            <Text style={[styles.baseText,styles.dimText]} >版本 {this.state.version}</Text>
+                            <Text style={[styles.baseText, styles.dimText]}>版本 {this.state.version}</Text>
                         </View>
 
-                        <View style={[styles.separatorHorizontal]} />
-                        <TouchableHighlight onPress={()=>this._gotoH5Page('功能说明','http://www.duoshouji.com/#mu-feature')}>
+                        <View style={[styles.separatorHorizontal]}/>
+                        <TouchableHighlight
+                            onPress={() => this._gotoH5Page('功能说明', 'http://www.duoshouji.com/#mu-feature')}>
                             <View style={styles.row}>
                                 <Text style={styles.text}>功能说明</Text>
                                 {chevronRightIcon}
                             </View>
                         </TouchableHighlight>
-                        <View style={styles.separatorHorizontal} />
+                        <View style={styles.separatorHorizontal}/>
 
-                        <TouchableHighlight onPress={()=>this._gotoH5Page('常见问题','http://www.duoshouji.com/#mu-faq')}>
+                        <TouchableHighlight
+                            onPress={() => this._gotoH5Page('常见问题', 'http://www.duoshouji.com/#mu-faq')}>
                             <View style={styles.row}>
                                 <Text style={styles.text}>常见问题</Text>
                                 {chevronRightIcon}
                             </View>
                         </TouchableHighlight>
-                        <View style={styles.separatorHorizontal} />
+                        <View style={styles.separatorHorizontal}/>
 
-                        <TouchableHighlight onPress={()=>this._gotoH5Page('联系我们','http://www.duoshouji.com/#mu-contact')}>
+                        <TouchableHighlight
+                            onPress={() => this._gotoH5Page('联系我们', 'http://www.duoshouji.com/#mu-contact')}>
                             <View style={styles.row}>
                                 <Text style={styles.text}>联系我们</Text>
                                 {chevronRightIcon}
                             </View>
                         </TouchableHighlight>
-                        <View style={styles.separatorHorizontal} />
+                        <View style={styles.separatorHorizontal}/>
                     </View>
-
 
 
                 </View>
