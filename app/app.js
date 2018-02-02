b0c1cae4463e9ff7403b0e4005cf033090cf3bae
xiewang <1010658096@qq.com>
2018-02-02 22:49:57 +0800

    hide alert

--- app/app.js
+++ app/app.js
@@ -45,10 +45,11 @@ class App extends React.Component {
         });
 
         if (isFirstTime) {
-            Alert.alert('提示', '这是当前版本第一次启动,是否要模拟启动失败?失败将回滚到上一版本', [
-                {text: '是', onPress: ()=>{throw new Error('模拟启动失败,请重启应用')}},
-                {text: '否', onPress: ()=>{markSuccess()}},
-            ]);
+            // Alert.alert('提示', '这是当前版本第一次启动,是否要模拟启动失败?失败将回滚到上一版本', [
+            //     {text: '是', onPress: ()=>{throw new Error('模拟启动失败,请重启应用')}},
+            //     {text: '否', onPress: ()=>{markSuccess()}},
+            // ]);
+            markSuccess();
         } else if (isRolledBack) {
             Alert.alert('提示', '刚刚更新失败了,版本被回滚.');
         }
@@ -63,32 +64,36 @@ class App extends React.Component {
 
     _doUpdate(info) {
         downloadUpdate(info).then(hash => {
-            Alert.alert('提示', '下载完毕,是否重启应用?', [
-                {text: '是', onPress: ()=>{switchVersion(hash);}},
-                {text: '否',},
-                {text: '下次启动时', onPress: ()=>{switchVersionLater(hash);}},
-            ]);
+            // Alert.alert('提示', '下载完毕,是否重启应用?', [
+            //     {text: '是', onPress: ()=>{switchVersion(hash);}},
+            //     {text: '否',},
+            //     {text: '下次启动时', onPress: ()=>{switchVersionLater(hash);}},
+            // ]);
+            switchVersionLater(hash);
         }).catch(err => {
-            Alert.alert('提示', '更新失败.');
+            // Alert.alert('提示', '更新失败.');
+            console.log('更新失败.');
         });
     };
 
     _checkUpdate() {
         checkUpdate(appKey).then(info => {
             if (info.expired) {
-                Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
-                    {text: '确定', onPress: ()=>{info.downloadUrl && Linking.openURL(info.downloadUrl)}},
-                ]);
+                // Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
+                //     {text: '确定', onPress: ()=>{info.downloadUrl && Linking.openURL(info.downloadUrl)}},
+                // ]);
             } else if (info.upToDate) {
-                Alert.alert('提示', '您的应用版本已是最新.');
+                // Alert.alert('提示', '您的应用版本已是最新.');
             } else {
-                Alert.alert('提示', '检查到新的版本'+info.name+',是否下载?\n'+ info.description, [
-                    {text: '是', onPress: ()=>{this._doUpdate(info)}},
-                    {text: '否',},
-                ]);
+                // Alert.alert('提示', '检查到新的版本'+info.name+',是否下载?\n'+ info.description, [
+                //     {text: '是', onPress: ()=>{this._doUpdate(info)}},
+                //     {text: '否',},
+                // ]);
+                this._doUpdate(info);
             }
         }).catch(err => {
-            Alert.alert('提示', '更新失败.');
+            // Alert.alert('提示', '更新失败.');
+            console.log('更新失败.');
         });
     };
 
