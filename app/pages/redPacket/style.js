a76ea6a29ea58f1b34627b4e8e044397246b6448
xiewang <1010658096@qq.com>
2018-03-07 19:36:07 +0800

    update context for error orders

--- app/pages/redPacket/style.js
+++ app/pages/redPacket/style.js
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         height: '100%',
-        backgroundColor: 'rgba(0,0,0,.2)',
+        backgroundColor: 'rgba(0,0,0,.08)',
         alignItems: 'center',
         justifyContent: 'center',
     },
