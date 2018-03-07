85b2e20953cfed131cb16001105d2ab0cc877878
xiewang <1010658096@qq.com>
2018-03-07 20:27:06 +0800

    update recent view's card style

--- app/pages/redPacket/style.js
+++ app/pages/redPacket/style.js
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         height: '100%',
-        backgroundColor: 'rgba(0,0,0,.08)',
+        backgroundColor: '#f0f0f0',
         alignItems: 'center',
         justifyContent: 'center',
     },
